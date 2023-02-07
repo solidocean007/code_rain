46554e271359e07b4d196a8f1de7f64572ede6c3
solidocean007 <clintonwilliams007@gmail.com>
2023-02-07 06:28:09 -0500

    debug resize

--- main.js
+++ main.js
@@ -1,30 +1,40 @@
-const canvas = document.getElementById('canvas1');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let gradient = ctx.createRadialGradient(canvas.width/2, canvas.height/2, 100, canvas.width/2, canvas.height/2, 200);
-gradient.addColorStop(0, 'red');
-gradient.addColorStop(0.2, 'yellow');
-gradient.addColorStop(0.4, 'green');
-gradient.addColorStop(0.6, 'cyan');
-gradient.addColorStop(0.8, 'blue');
-gradient.addColorStop(1, 'magenta');
+let gradient = ctx.createRadialGradient(
+  canvas.width / 2,
+  canvas.height / 2,
+  100,
+  canvas.width / 2,
+  canvas.height / 2,
+  200
+);
+gradient.addColorStop(0, "red");
+gradient.addColorStop(0.2, "yellow");
+gradient.addColorStop(0.4, "green");
+gradient.addColorStop(0.6, "cyan");
+gradient.addColorStop(0.8, "blue");
+gradient.addColorStop(1, "magenta");
 
 class Symbol {
-  constructor(x, y, fontSize, canvasHeight){
-    this.characters = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトホモヨョロヲゴゾドボポヴッン0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  constructor(x, y, fontSize, canvasHeight) {
+    this.characters =
+      "アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトホモヨョロヲゴゾドボポヴッン0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     this.x = x;
     this.y = y;
     this.fontSize = fontSize;
-    this.text = '';
+    this.text = "";
     this.canvasHeight = canvasHeight;
   }
-  draw(context){
-    this.text = this.characters.charAt(Math.floor(Math.random() * this.characters.length));
-    context.fillStyle = '#0aff0a';
+  draw(context) {
+    this.text = this.characters.charAt(
+      Math.floor(Math.random() * this.characters.length)
+    );
+    context.fillStyle = "#0aff0a";
     context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
-    if(this.y * this.fontSize > this.canvasHeight && Math.random() > 0.98){
+    if (this.y * this.fontSize > this.canvasHeight && Math.random() > 0.98) {
       this.y = 0;
     } else {
       this.y += 1;
@@ -33,24 +43,24 @@ class Symbol {
 }
 
 class Effect {
-  constructor(canvasWidth, canvasHeight){
+  constructor(canvasWidth, canvasHeight) {
     this.canvasWidth = canvasWidth;
     this.canvasHeight = canvasHeight;
     this.fontSize = 25;
-    this.columns = this.canvasWidth/this.fontSize;
+    this.columns = this.canvasWidth / this.fontSize;
     this.symbols = [];
     this.#initialize();
     console.log(this.symbols);
   }
-  #initialize(){
-    for (let i = 0; i < this.columns; i++){
+  #initialize() {
+    for (let i = 0; i < this.columns; i++) {
       this.symbols[i] = new Symbol(i, 0, this.fontSize, this.canvasHeight);
     }
   }
-  resize(width, height){
+  resize(width, height) {
     this.canvasWidth = width;
     this.canvasHeight = height;
-    this.columns = this.canvasWidth/this.fontSize;
+    this.columns = this.canvasWidth / this.fontSize;
     this.symbols = [];
     this.#initialize();
   }
@@ -59,19 +69,19 @@ class Effect {
 const effect = new Effect(canvas.width, canvas.Height);
 let lastTime = 0;
 const fps = 30;
-const nextFrame = 1000/fps;
+const nextFrame = 1000 / fps;
 let timer = 0;
 
-function animate(timeStamp){
+function animate(timeStamp) {
   const deltaTime = timeStamp - lastTime;
   lastTime = timeStamp;
-  if(timer > nextFrame){
-    ctx.fillStyle = 'rgba(0,0,0,0.05)';
-    ctx.textAlign = 'center';
+  if (timer > nextFrame) {
+    ctx.fillStyle = "rgba(0,0,0,0.05)";
+    ctx.textAlign = "center";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = gradient; //'#0aff0a';
-    ctx.font = effect.fontSize + 'px monospace';
-    effect.symbols.forEach(symbol => symbol.draw(ctx));
+    ctx.font = effect.fontSize + "px monospace";
+    effect.symbols.forEach((symbol) => symbol.draw(ctx));
     timer = 0;
   } else {
     timer += deltaTime;
@@ -80,8 +90,8 @@ function animate(timeStamp){
 }
 animate(0);
 
-window.addEventListener('resize', function(){
-  canvas.width = this.window.innerWidth;
-  canvas.height = this.window.innerHeight;
+window.addEventListener("resize", function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
   effect.resize(canvas.width, canvas.height);
 });
